73af4cfaaa678f43571754d8be6ef6d00e7ed33f
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-06 10:58:32 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     float per;     per=(a+b+c+d+e)*100/500.0;     if(per>=90)     { printf("Grade A"); }     else if(per>=80)     { printf("Grade B"); }     else if(per>=70)     { printf("Grade C"); }     else if(per>=60)     { printf("Grade D"); }     else if(per>=40)     { printf("Grade E"); }     else     { printf("Grade F"); } }

---
+++ Grades.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    float per;
+    per=(a+b+c+d+e)*100/500.0;
+    if(per>=90)
+    { printf("Grade A"); }
+    else if(per>=80)
+    { printf("Grade B"); }
+    else if(per>=70)
+    { printf("Grade C"); }
+    else if(per>=60)
+    { printf("Grade D"); }
+    else if(per>=40)
+    { printf("Grade E"); }
+    else
+    { printf("Grade F"); }
+}
