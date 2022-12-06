22e062e92b502b4a88a338775f6c208136da0795
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-06 11:33:37 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     float c;     if (n<200)     { c=1.20; }     else if (n>=200&&n<400)     { c=1.50; }     else if (n>=400&&n<600)     { c=1.80; }     else     { c=2.00; }     float b,sc,amt;     b=n*c;     if (b>400)     { sc=b*0.15; }     else     { sc=100; }     amt=b+sc;     printf("%0.2f",amt); }

---
+++ Current_bill.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    float c;
+    if (n<200)
+    { c=1.20; }
+    else if (n>=200&&n<400)
+    { c=1.50; }
+    else if (n>=400&&n<600)
+    { c=1.80; }
+    else 
+    { c=2.00; }
+    float b,sc,amt;
+    b=n*c;
+    if (b>400)
+    { sc=b*0.15; }
+    else
+    { sc=100; }
+    amt=b+sc;
+    printf("%0.2f",amt);
+}
