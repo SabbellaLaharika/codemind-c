90e02bd016825b64220d2a8335bedc63a54c6568
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-07 20:58:26 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=i;j++)         {             if(j==1||i==n||j==i)             printf("*");             else             printf(" ");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,14 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=n;i>=1;i--)
+    for(int i=1;i<=n;i++)
     {
-        for(int j=n;j>=1;j--)
+        for(int j=1;j<=i;j++)
         {
-            printf("%d ",j);
+            if(j==1||i==n||j==i)
+            printf("*");
+            else
+            printf(" ");
         }
         printf("
 ");
