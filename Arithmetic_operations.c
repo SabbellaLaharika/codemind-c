94048b5f7d0e95837c74d2e4c4af8e18fb32ccdc
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-11 20:51:51 +0530

    //Arithmetic Operations #include<stdio.h> int main() {     int a,b;     scanf("%d",&a);     scanf("%d",&b);     int s,d,p,q,r;     s=a+b;     d=a-b;     p=a*b;     q=a/b;     r=a%b;     printf("Sum:%d Difference:%d Product:%d Quotient:%d Remainder:%d",s,d,p,q,r); }

---
+++ Arithmetic_operations.c
@@ -0,0 +1,19 @@
+//Arithmetic Operations
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int s,d,p,q,r;
+    s=a+b;
+    d=a-b;
+    p=a*b;
+    q=a/b;
+    r=a%b;
+    printf("Sum:%d
+Difference:%d
+Product:%d
+Quotient:%d
+Remainder:%d",s,d,p,q,r);
+}
