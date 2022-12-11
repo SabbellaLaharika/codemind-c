d3a03b6058a41220dbfe043f700a874708469c6b
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-11 20:50:27 +0530

    //Weight of the third one #include<stdio.h> int main() {     int x,a,b;     scanf("%d",&x);     scanf("%d",&a);     scanf("%d",&b);     int c;     c = 3*x-a-b;     printf("%d",c); }

---
+++ Average_weight.c
@@ -0,0 +1,12 @@
+//Weight of the third one
+#include<stdio.h>
+int main()
+{
+    int x,a,b;
+    scanf("%d",&x);
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int c;
+    c = 3*x-a-b;
+    printf("%d",c);
+}
