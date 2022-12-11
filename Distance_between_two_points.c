3e5572aab0a7593368b5fb11678aa40bfe6b33b9
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-11 20:53:25 +0530

    //Distance between two points #include<stdio.h> #include<math.h> int main() {     int x1,y1,x2,y2;     scanf("%d",&x1);     scanf("%d",&y1);     scanf("%d",&x2);     scanf("%d",&y2);     float dis=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));     printf("%0.4f",dis); }

---
+++ Distance_between_two_points.c
@@ -0,0 +1,13 @@
+//Distance between two points
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x1,y1,x2,y2;
+    scanf("%d",&x1);
+    scanf("%d",&y1);
+    scanf("%d",&x2);
+    scanf("%d",&y2);
+    float dis=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    printf("%0.4f",dis);
+}
