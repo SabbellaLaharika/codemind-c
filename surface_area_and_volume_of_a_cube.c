cd21e652fdcc234991c5c19033e91f29b0d8caa9
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-14 19:36:52 +0530

    //Surface Area and Volume of a Cube #include<stdio.h> int main() {     float a;     scanf("%f",&a);     float sa,vol;     sa=6*a*a;     vol=a*a*a;     printf("Surface area = %0.0f and Volume = %0.0f",sa,vol); }

---
+++ surface_area_and_volume_of_a_cube.c
@@ -0,0 +1,11 @@
+//Surface Area and Volume of a Cube
+#include<stdio.h>
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    float sa,vol;
+    sa=6*a*a;
+    vol=a*a*a;
+    printf("Surface area = %0.0f and Volume = %0.0f",sa,vol);
+}
