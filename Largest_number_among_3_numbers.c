da7d24272dbab3a9d2859233ad36eb389f6469b3
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-17 20:59:08 +0530

    //Largest of three numbers #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>b)     {         if(a>c)         { printf("%d",a); }         else         { printf("%d",c); }     }     else     {         if(c>b)         { printf("%d",c); }         else         { printf("%d",b); }     } }

---
+++ Largest_number_among_3_numbers.c
@@ -0,0 +1,21 @@
+//Largest of three numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+        { printf("%d",a); }
+        else
+        { printf("%d",c); }
+    }
+    else
+    {
+        if(c>b)
+        { printf("%d",c); }
+        else
+        { printf("%d",b); }
+    }
+}
