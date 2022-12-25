421a6383c96170028540f722faf2c9c8acb6c9bf
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-25 16:57:35 +0530

    #include<stdio.h> int main() {     int i,n,a,b;     scanf("%d%d%d",&n,&a,&b);     for(i=a;i<=b;i++)     {         int mt;         mt=n*i;         printf("%d x %d = %d ",n,i,mt);     } }

---
+++ Multiplication_Table_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a,b;
+    scanf("%d%d%d",&n,&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        int mt;
+        mt=n*i;
+        printf("%d x %d = %d
+",n,i,mt);
+    }
+}
