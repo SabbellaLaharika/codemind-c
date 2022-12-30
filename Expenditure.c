2ce632870895b52e233777c823f0f62b9a0eba4a
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2022-12-30 21:40:51 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d",&x);     scanf("%d",&y);     if(x>30*y)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Expenditure.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    if(x>30*y)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
